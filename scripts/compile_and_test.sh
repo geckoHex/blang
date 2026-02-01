7a2dcd3aadde488f38f2fb78ac51e0703e5587da
geckoHex <123674650+geckoHex@users.noreply.github.com>
2026-01-31 16:29:46 -0800

    Update name of file_in implementation in test script

--- scripts/compile_and_test.sh
+++ scripts/compile_and_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ src/main.cpp src/util/logger.cpp src/io/filein.cpp -o build/blang
+g++ src/main.cpp src/util/logger.cpp src/io/file_in.cpp -o build/blang
 ./build/blang examples/program.blang
